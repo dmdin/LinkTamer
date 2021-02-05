6378b18c196a9f12b47d8878dce1bb3fa3146ba6
Dikower <dmitriy1d01@gmail.com>
2021-02-05 21:55:52 +0300

    Migrating to Go backend

---
+++ backend/go.mod
@@ -0,0 +1,11 @@
+module github.com/Dikower/LinkTamer/backend
+
+go 1.16
+
+require (
+	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/gofiber/fiber/v2 v2.4.1
+	github.com/klauspost/compress v1.11.7 // indirect
+	github.com/valyala/fasthttp v1.19.0 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(logger.New())
+	// give response when at /
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "You are at the endpoint 😉",
+		})
+	})
+
+	// Listen on server 8000 and catch error if any
+	err := app.Listen(":8000")
+
+	// handle error
+	if err != nil {
+		panic(err)
+	}
+}
